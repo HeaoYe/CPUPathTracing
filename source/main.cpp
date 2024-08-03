dce3a9e51c68a47ec57b9e94011221f7a5c53f90
HeaoYe <1697427528@qq.com>
2024-08-04 02:39:04 +0800

    第一课

---
+++ source/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <glm/glm.hpp>
+
+int main() {
+    glm::vec3 test = { 1, 2, 6 };
+    std::cout << "Hello World !" << std::endl;
+    std::cout << test.x << test.y << test.z << std::endl;
+    return 0;
+}
