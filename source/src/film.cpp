e2967483e717d79c929fa41786eb3ae16c81fab4
HeaoYe <1697427528@qq.com>
2024-08-07 01:41:34 +0800

    第三讲：自旋锁与并行for循环

--- source/src/film.cpp
+++ source/src/film.cpp
@@ -6,14 +6,14 @@ Film::Film(size_t width, size_t height) : width(width), height(height) {
 }
 
 void Film::save(const std::filesystem::path &filename) {
-    std::ofstream file(filename);
-    file << "P3\n" << width << ' ' << height << "\n255\n";
+    std::ofstream file(filename, std::ios::binary);
+    file << "P6\n" << width << ' ' << height << "\n255\n";
 
     for (size_t y = 0; y < height; y ++) {
         for (size_t x = 0; x < width; x ++) {
             const glm::vec3 &color = getPixel(x, y);
             glm::ivec3 color_i = glm::clamp(color * 255.f, 0.f, 255.f);
-            file << color_i.x << ' ' << color_i.y << ' ' << color_i.z << '\n';
+            file << static_cast<uint8_t>(color_i.x) << static_cast<uint8_t>(color_i.y) << static_cast<uint8_t>(color_i.z);
         }
     }
 }
