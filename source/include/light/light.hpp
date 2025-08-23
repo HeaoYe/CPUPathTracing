2a9146d49fc4ee88f4b91a5fd1a9e2db69e8c174
HeaoYe <1697427528@qq.com>
2025-08-24 07:47:47 +0800

    第20讲：BVH构建优化

--- source/include/light/light.hpp
+++ source/include/light/light.hpp
@@ -11,21 +11,13 @@ struct LightSample {
     float pdf;
 };
 
-enum class LightType {
-    eAreaLight,
-    eInfiniteLight,
-};
-
 class Light {
 public:
-    Light(const glm::vec3 &Le) : Le(Le) {}
+    virtual bool impossible() const = 0;
 
-    virtual LightType getType() const = 0;
     virtual float Phi(float scene_radius) const = 0;
     virtual std::optional<LightSample> sampleLight(const glm::vec3 &surface_point, float scene_radius, const RNG &rng, bool allow_mis_compensation) const = 0;
 
-    virtual glm::vec3 getRadiance(const glm::vec3 &surface_point, const glm::vec3 &light_point, const glm::vec3 &normal) const { return Le; }
+    virtual glm::vec3 getRadiance(const glm::vec3 &surface_point, const glm::vec3 &light_point, const glm::vec3 &normal) const = 0;
     virtual float getPDF(const glm::vec3 &surface_point, const glm::vec3 &light_point, const glm::vec3 &normal, bool allow_mis_compensation) const = 0;
-protected:
-    glm::vec3 Le;
 };
